deb888ad6e502bc8324529f7b106fff4c7bcdb4c
roy <rcdosado@gmail.com>
2016-09-17 10:25:45 +0800

    pass PsKillCOmmandTest::testCommand

--- src/PsKillWrapper/PsKillCommand.php
+++ src/PsKillWrapper/PsKillCommand.php
@@ -82,12 +82,10 @@ class PsKillCommand
      * the Pskill command line utility. If the last argument is an array, it is
      * passed as the command options.
      *
-     * @param string $command
-     *   The Pskill command being run
-     * @param string ...
+     * @internal param string $command The Pskill command being run*   The Pskill command being run
+     * @internal param $string ...
      *   Zero or more arguments passed to the Pskill command.
-     * @param array $options
-     *   An optional array of arguments to pass to the command.
+     * @internal param array $options An optional array of arguments to pass to the command.*   An optional array of arguments to pass to the command.
      *
      * @return \PsKillWrapper\PsKillCommand
      */
@@ -147,5 +145,154 @@ class PsKillCommand
         $this->bypass = (bool) $bypass;
         return $this;
     }
+
+    /**
+     * Returns true if the Git command should be run.
+     *
+     * The return value is the boolean opposite $this->bypass. Although this
+     * seems complex, it makes the code more readable when checking whether the
+     * command should be run or not.
+     *
+     * @return boolean
+     *   If true, the command should be run.
+     */
+    public function notBypassed()
+    {
+        return !$this->bypass;
+    }
+
+    /**
+     * Builds the command line options for use in the PsKill command.
+     *
+     * @return string
+     */
+    public function buildOptions()
+    {
+        $options = array();
+        foreach ($this->options as $option => $values) {
+            foreach ((array) $values as $value) {
+                $prefix = (strlen($option) != 1) ? '--' : '-';
+                $rendered = $prefix . $option;
+                if ($value !== true) {
+                    $rendered .= ('--' == $prefix) ? '=' : ' ';
+                    $rendered .= ProcessUtils::escapeArgument($value);
+                }
+                $options[] = $rendered;
+            }
+        }
+        return join(' ', $options);
+    }
+    /**
+     * Sets a command line option.
+     *
+     * Option names are passed as-is to the command line, whereas the values are
+     * escaped using \Symfony\Component\Process\ProcessUtils.
+     *
+     * @param string $option
+     *   The option name, e.g. "branch", "q".
+     * @param string|true $value
+     *   The option's value, pass true if the options is a flag.
+     *
+     * @return $this
+     *
+     */
+    public function setOption($option, $value)
+    {
+        $this->options[$option] = $value;
+        return $this;
+    }
+
+    /**
+     * Sets multiple command line options.
+     *
+     * @param array $options
+     *   An associative array of command line options.
+     *
+     * @return $this
+     *
+     */
+    public function setOptions(array $options)
+    {
+        foreach ($options as $option => $value) {
+            $this->setOption($option, $value);
+        }
+        return $this;
+    }
+
+    /**
+     * Sets a command line flag.
+     *
+     * @param $option
+     * @internal param string $flag The flag name, e.g. "q", "a".*   The flag name, e.g. "q", "a".
+     *
+     * @return \PsKillWrapper\PsKillCommand
+     *
+     * @see \PsKillWrapper\PsKillCommand::setOption()
+     */
+    public function setFlag($option)
+    {
+        return $this->setOption($option, true);
+    }
+
+    /**
+     * Gets a command line option.
+     *
+     * @param string $option
+     *   The option name, e.g. "branch", "q".
+     * @param mixed $default
+     *   Value that is returned if the option is not set, defaults to null.
+     *
+     * @return mixed
+     */
+    public function getOption($option, $default = null)
+    {
+        return (isset($this->options[$option])) ? $this->options[$option] : $default;
+    }
+
+    /**
+     * Unsets a command line option.
+     *
+     * @param string $option
+     *   The option name, e.g. "branch", "q".
+     *
+     * @return \PsKillWrapper\PsKillCommand
+     */
+    public function unsetOption($option)
+    {
+        unset($this->options[$option]);
+        return $this;
+    }
+
+    /**
+     * Adds a command line argument passed to the PsKill command.
+     *
+     * @param string $arg
+     *   The argument, e.g. the repo URL, directory, etc.
+     *
+     * @return \PsKillWrapper\PsKillCommand
+     */
+    public function addArgument($arg)
+    {
+        $this->args[] = $arg;
+        return $this;
+    }
+    /**
+     * Renders the arguments and options for the PsKill command.
+     *
+     * @return string
+     *
+     * @see PsKillCommand::getCommand()
+     * @see PsKillCommand::buildOptions()
+     */
+    public function getCommandLine()
+    {
+        $command = array(
+            $this->getCommand(),
+            $this->buildOptions(),
+            join(' ', array_map(array('\Symfony\Component\Process\ProcessUtils', 'escapeArgument'), $this->args)),
+        );
+        return join(' ', array_filter($command));
+    }
+
 
 }
--- test/PsKillWrapper/Test/PsKillCommandTest.php
+++ test/PsKillWrapper/Test/PsKillCommandTest.php
@@ -10,8 +10,27 @@ namespace PsKillWrapper\Test;
 
 use PsKillWrapper\PsKillCommand;
 
+include_once("PsKillWrapperTestCase.php");
+
 class PsKillCommandTest extends PsKillWrapperTestCase
 {
 
+    public function testCommand(){
+        $command = $this->randomString();
+        $argument = $this->randomString();
+        $flag = $this->randomString();
+        $optionName = $this->randomString();
+        $optionValue = $this->randomString();
 
+        $pskill = PsKillCommand::getInstance($command)
+            ->addArgument($argument)
+            ->setFlag($flag)
+            ->setOption($optionName, $optionValue);
+
+        $expected = "$command --$flag --$optionName=\"$optionValue\" \"$argument\"";
+        $commandLine = $pskill->getCommandLine();
+
+        $this->assertEquals($expected, $commandLine);
+//        $this->assertEquals(1, 1);
+    }
 } 

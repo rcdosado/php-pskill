c956291c2df80d40db748e65cdfcaa42f186e343
roy <rcdosado@gmail.com>
2016-09-18 22:39:25 +0800

    Add PsKillProcess class

---
+++ src/PsKillWrapper/Event/PsKillOutputEvent.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: m1k3y
+ * Date: 9/18/2016
+ * Time: 10:33 PM
+ */
+
+namespace PsKillWrapper\Event;
+
+
+class PsKillOutputEvent {
+
+} 
--- src/PsKillWrapper/PsKillProcess.php
+++ src/PsKillWrapper/PsKillProcess.php
@@ -8,7 +8,50 @@
 
 namespace PsKillWrapper;
 
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessUtils;
 
-class PsKillProcess {
+
+class PsKillProcess extends Process{
+
+    protected $pskill;
+
+    protected $command;
+
+    /**
+     * Constructs a PsKillProcess object.
+     * @param \PsKillWrapper\PsKillWrapper|PsKillWrapper $pskill
+     * @param \PsKillWrapper\PsKillCommand|PsKillCommand $command
+     * @param string|null $cwd
+     * @throws PsKillException
+     */
+    public function __construct(PsKillWrapper $pskill, PsKillCommand $command, $cwd = null)
+    {
+        $this->pskill = $pskill;
+        $this->command = $command;
+
+        // Build the command line options, flags, and arguments.
+        $binary = ProcessUtils::escapeArgument($pskill->getPsKillBinary());
+        $commandLine = rtrim($binary . ' ' . $command->getCommandLine());
+
+        // Resolve the working directory of the PsKill process. Use the directory
+        // in the command object if it exists.
+        if (null === $cwd) {
+            if (null !== $directory = $command->getDirectory()) {
+                if (!$cwd = realpath($directory)) {
+                    throw new PsKillException('Path to working directory could not be resolved: ' . $directory);
+                }
+            }
+        }
+
+        // Finalize the environment variables, an empty array is converted
+        // to null which enherits the environment of the PHP process.
+//        $env = $pskill->getEnvVars();
+//        if (!$env) {
+//            $env = null;
+//        }
+        $env = "";
+        parent::__construct($commandLine, $cwd, $env, null, $pskill->getTimeout(), $pskill->getProcOptions());
+    }
 
 } 
